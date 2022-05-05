2f1b83453f254c2af1cee6b694a25e99e6d8eb4d
Diego Estrada <diegoestrada@MacBook-Air-de-Diego.local>
2022-05-04 23:32:38 -0600

    testing three

--- leaf.py
+++ leaf.py
@@ -20,14 +20,14 @@ class Leaf():
 
     def add_first_pos(self):
         if self.is_op:
-            if self.name == '∪':
+            if self.name == '∪': # In case is OR
                 self.first_pos = self.leaf_child[0].first_pos + self.leaf_child[1].first_pos
-            elif self.name == '∩':
+            elif self.name == '∩': # In case is CONCAT
                 if self.leaf_child[0].is_nullable:
                     self.first_pos = self.leaf_child[0].first_pos + self.leaf_child[1].first_pos
                 else:
                     self.first_pos += self.leaf_child[0].first_pos
-            elif self.name == 'Δ':
+            elif self.name == 'Δ': # In case is KLEENE
                 self.first_pos += self.leaf_child[0].first_pos
         else:
             if self.name != 'ε':
@@ -35,14 +35,14 @@ class Leaf():
 
     def add_last_pos(self):
         if self.is_op:
-            if self.name == '∪':
+            if self.name == '∪': #In case is OR
                 self.last_pos = self.leaf_child[0].last_pos + self.leaf_child[1].last_pos
-            elif self.name == '∩':
+            elif self.name == '∩':#In case is CONCAT
                 if self.leaf_child[1].is_nullable:
                     self.last_pos = self.leaf_child[0].last_pos + self.leaf_child[1].last_pos
                 else:
                     self.last_pos += self.leaf_child[1].last_pos
-            elif self.name == 'Δ':
+            elif self.name == 'Δ': #In case is Kleene
                 self.last_pos += self.leaf_child[0].last_pos
         else:
             if self.name != 'ε':
